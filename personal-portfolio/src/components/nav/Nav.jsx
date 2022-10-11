b0b50f82a4c9ad5c893c8eda38d1e867653eaf95
Zaidi <zaidiahsan.r@gmail.com>
2022-10-10 20:23:55 -0500

    floating nav

--- personal-portfolio/src/components/nav/Nav.jsx
+++ personal-portfolio/src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import { AiFillHome } from 'react-icons/ai'
+import { FaUserTie } from 'react-icons/fa'
+import { BsBookHalf } from 'react-icons/bs'
+import { RiServiceLine } from 'react-icons/ri'
+import { AiOutlineContacts } from 'react-icons/ai'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' className='active'><AiFillHome /></a>
+      <a href='#about'><FaUserTie /></a>
+      <a href='#experience'><BsBookHalf /></a>
+      <a href='#services'><RiServiceLine /></a>
+      <a href='#contact'><AiOutlineContacts /></a>
+    </nav>
   )
 }
 
