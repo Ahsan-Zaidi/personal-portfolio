b5c4280b3dfa64f6d2e8299bed92a3d9a1f0cba0
Zaidi <zaidiahsan.r@gmail.com>
2022-10-27 15:01:18 -0500

    updated resume

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ import { FiDribbble } from 'react-icons/fi'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://linkedin.com' target="_blank"><BsLinkedin /></a>
+        <a href='https://www.linkedin.com/in/ahsan-zaidi-25641b254/' target="_blank"><BsLinkedin /></a>
         <a href='https://github.com' target="_blank"><FiGithub /></a>
         <a href='https://dribbble.com' target="_blank"><FiDribbble /></a>
     </div>
