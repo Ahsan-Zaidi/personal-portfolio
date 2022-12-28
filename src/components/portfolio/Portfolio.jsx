165d926421344c8f22556d59526c5789b462d764
Zaidi <zaidiahsan.r@gmail.com>
2022-12-28 16:59:46 -0600

    Addedproject

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,6 +6,7 @@ import Image3 from '../../assets/image3.jpg'
 import Image4 from '../../assets/image4.jpg'
 import Image5 from '../../assets/image5.jpg'
 import Image6 from '../../assets/image6.jpg'
+import Image7 from '../../assets/image7.jpg'
 
 const portfolio = () => {
   return (
@@ -79,6 +80,17 @@ const portfolio = () => {
             <a href='https://note-takers01.herokuapp.com/' className='btn btn-primary' target='_blank'>Live Demo</a>
           </div>
         </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={Image7} alt=""></img>
+          </div>
+          <h3>Restaurant Temp</h3>
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com/Ahsan-Zaidi/Restaurant' className='btn' target='_blank'>GitHub</a>
+          </div>
+        </article>
+
       </div>
     </section>
   )
