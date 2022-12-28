d23ef913b286285f90bbcc9f3d4e8ddd6974df57
Zaidi <zaidiahsan.r@gmail.com>
2022-12-28 16:49:38 -0600

    updated github

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import Me from '../../assets/Me.svg'
 import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,7 +7,7 @@ const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href='https://www.linkedin.com/in/ahsan-zaidi-25641b254/' target="_blank"><BsLinkedin /></a>
-        <a href='https://github.com' target="_blank"><FiGithub /></a>
+        <a href='https://github.com/Ahsan-Zaidi' target="_blank"><FiGithub /></a>
         <a href='https://dribbble.com' target="_blank"><FiDribbble /></a>
     </div>
   )
